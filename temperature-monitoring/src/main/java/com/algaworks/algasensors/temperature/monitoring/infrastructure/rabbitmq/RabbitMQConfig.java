997f8662326dde9c57d9281aaa265e194542743b
Tony Costa <tonyblack10@gmail.com>
2025-04-20 02:42:31 -0300

    add dead letter queue configuration for temperature processing in RabbitMQ

--- temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
+++ temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.ExchangeBuilder;
@@ -16,7 +17,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-  public static final String QUEUE_PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1.q";
+  private static final String PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1";
+  public static final String QUEUE_PROCESS_TEMPERATURE = PROCESS_TEMPERATURE + ".q";
+  public static final String DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE = PROCESS_TEMPERATURE + ".dlq";
   public static final String QUEUE_ALERTING = "temperature-monitoring.alerting.v1.q";
 
   @Bean
@@ -31,7 +34,17 @@ public class RabbitMQConfig {
 
   @Bean
   public Queue queueProcessTemperature() {
-    return QueueBuilder.durable(QUEUE_PROCESS_TEMPERATURE).build();
+    Map<String, Object> args = Map.of(
+      "x-dead-letter-exchange", "",
+      "x-dead-letter-routing-key", DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE
+    );
+
+    return QueueBuilder.durable(QUEUE_PROCESS_TEMPERATURE).withArguments(args).build();
+  }
+
+  @Bean
+  public Queue deadLetterQueueProcessTemperature() {
+    return QueueBuilder.durable(DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE).build();
   }
 
   @Bean
