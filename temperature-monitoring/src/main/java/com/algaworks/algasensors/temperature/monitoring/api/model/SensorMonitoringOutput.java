c0db2b6a85567458caf16b02d0efe7dea01079e6
Tony Costa <tonyblack10@gmail.com>
2025-04-19 23:04:06 -0300

    add getDetail method to SensorMonitoringClient and implement SensorDetailOutput for detailed sensor information

--- temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/api/model/SensorMonitoringOutput.java
+++ temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/api/model/SensorMonitoringOutput.java
@@ -5,43 +5,45 @@ import io.hypersistence.tsid.TSID;
 import java.time.OffsetDateTime;
 
 public record SensorMonitoringOutput(
-        TSID id,
-        Double lastTemperature,
-        OffsetDateTime updatedAt,
-        Boolean enabled
+    TSID id,
+    Double lastTemperature,
+    OffsetDateTime updatedAt,
+    Boolean enabled
 ) {
-    public static SensorMonitoringOutputBuilder builder() {
-        return new SensorMonitoringOutputBuilder();
+
+  public static SensorMonitoringOutputBuilder builder() {
+    return new SensorMonitoringOutputBuilder();
+  }
+
+  public static class SensorMonitoringOutputBuilder {
+
+    private TSID id;
+    private Double lastTemperature;
+    private OffsetDateTime updatedAt;
+    private Boolean enabled;
+
+    public SensorMonitoringOutputBuilder id(TSID id) {
+      this.id = id;
+      return this;
     }
 
-    public static class SensorMonitoringOutputBuilder {
-        private TSID id;
-        private Double lastTemperature;
-        private OffsetDateTime updatedAt;
-        private Boolean enabled;
-
-        public SensorMonitoringOutputBuilder id(TSID id) {
-            this.id = id;
-            return this;
-        }
-
-        public SensorMonitoringOutputBuilder lastTemperature(Double lastTemperature) {
-            this.lastTemperature = lastTemperature;
-            return this;
-        }
-
-        public SensorMonitoringOutputBuilder updatedAt(OffsetDateTime updatedAt) {
-            this.updatedAt = updatedAt;
-            return this;
-        }
-
-        public SensorMonitoringOutputBuilder enabled(Boolean enabled) {
-            this.enabled = enabled;
-            return this;
-        }
-
-        public SensorMonitoringOutput build() {
-            return new SensorMonitoringOutput(id, lastTemperature, updatedAt, enabled);
-        }
+    public SensorMonitoringOutputBuilder lastTemperature(Double lastTemperature) {
+      this.lastTemperature = lastTemperature;
+      return this;
     }
+
+    public SensorMonitoringOutputBuilder updatedAt(OffsetDateTime updatedAt) {
+      this.updatedAt = updatedAt;
+      return this;
+    }
+
+    public SensorMonitoringOutputBuilder enabled(Boolean enabled) {
+      this.enabled = enabled;
+      return this;
+    }
+
+    public SensorMonitoringOutput build() {
+      return new SensorMonitoringOutput(id, lastTemperature, updatedAt, enabled);
+    }
+  }
 }
