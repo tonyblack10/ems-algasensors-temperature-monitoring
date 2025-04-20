ea8a7cfee8e6364cf187a6a9e08fa81609a70810
Tony Costa <tonyblack10@gmail.com>
2025-04-20 01:52:26 -0300

    add TemperatureMonitoringService to process temperature readings and integrate with RabbitMQ listener

---
+++ temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/domain/service/TemperatureMonitoringService.java
@@ -0,0 +1,65 @@
+package com.algaworks.algasensors.temperature.monitoring.domain.service;
+
+import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
+import com.algaworks.algasensors.temperature.monitoring.domain.model.SensorId;
+import com.algaworks.algasensors.temperature.monitoring.domain.model.SensorMonitoring;
+import com.algaworks.algasensors.temperature.monitoring.domain.model.TemperatureLog;
+import com.algaworks.algasensors.temperature.monitoring.domain.model.TemperatureLogId;
+import com.algaworks.algasensors.temperature.monitoring.domain.repository.SensorMonitoringRepository;
+import com.algaworks.algasensors.temperature.monitoring.domain.repository.TemperatureLogRepository;
+import jakarta.transaction.Transactional;
+import java.time.OffsetDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemperatureMonitoringService {
+
+  private static final Logger log = LoggerFactory.getLogger(TemperatureMonitoringService.class);
+
+  private final SensorMonitoringRepository sensorMonitoringRepository;
+  private final TemperatureLogRepository temperatureLogRepository;
+
+  public TemperatureMonitoringService(SensorMonitoringRepository sensorMonitoringRepository,
+      TemperatureLogRepository temperatureLogRepository) {
+    this.sensorMonitoringRepository = sensorMonitoringRepository;
+    this.temperatureLogRepository = temperatureLogRepository;
+  }
+
+  @Transactional
+  public void processTemperatureReading(TemperatureLogData temperatureLogData) {
+
+    sensorMonitoringRepository.findById(new SensorId(temperatureLogData.sensorId()))
+        .ifPresentOrElse(
+            sensor -> handleSensorMonitoring(temperatureLogData, sensor),
+            () -> logIgnoredTemperature(temperatureLogData));
+  }
+
+  private void handleSensorMonitoring(TemperatureLogData temperatureLogData,
+      SensorMonitoring sensor) {
+    if (sensor.getEnabled()) {
+      sensor.setLastTemperature(temperatureLogData.value());
+      sensor.setUpdatedAt(OffsetDateTime.now());
+      sensorMonitoringRepository.save(sensor);
+
+      TemperatureLog temperatureLog = TemperatureLog.builder()
+          .id(new TemperatureLogId(temperatureLogData.id()))
+          .registeredAt(temperatureLogData.registeredAt())
+          .value(temperatureLogData.value())
+          .sensorId(new SensorId(temperatureLogData.sensorId()))
+          .build();
+
+      temperatureLogRepository.save(temperatureLog);
+      log.info("Temperature Updated: SensorId {} Temp {}", temperatureLogData.sensorId(),
+          temperatureLogData.value());
+    } else {
+      logIgnoredTemperature(temperatureLogData);
+    }
+  }
+
+  private void logIgnoredTemperature(TemperatureLogData temperatureLogData) {
+    log.info("Temperature Ignored: SensorId {} Temp {}", temperatureLogData.sensorId(),
+        temperatureLogData.value());
+  }
+}
--- temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ temperature-monitoring/src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -3,6 +3,7 @@ package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq
 import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE_NAME;
 
 import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
+import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,11 +17,19 @@ public class RabbitMQListener {
 
   private static final Logger log = LoggerFactory.getLogger(RabbitMQListener.class);
 
+  private final TemperatureMonitoringService temperatureMonitoringService;
+
+  public RabbitMQListener(TemperatureMonitoringService temperatureMonitoringService) {
+    this.temperatureMonitoringService = temperatureMonitoringService;
+  }
+
   @RabbitListener(queues = QUEUE_NAME)
   public void handle(@Payload TemperatureLogData temperatureLogData,
       @Headers Map<String, Object> headers) {
     log.info("Received temperature log data: {}", temperatureLogData);
     log.info("Headers: {}", headers);
+
+    temperatureMonitoringService.processTemperatureReading(temperatureLogData);
   }
 
 }
